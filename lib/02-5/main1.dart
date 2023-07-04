4e2d9668e24dcb3835da527b6d3ec15aa909ea69
jhhan <hhan@interezen.co.kr>
2023-07-04 09:15:30 +0900

    02-5 구구단 프로그램 도전! 다트 프로그램 만들기 23.07.24

--- lib/02-5/main1.dart
+++ lib/02-5/main1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async';
 
-void main1 ()  {
+void main ()  {
   for(int i =2; i<10; i++){
     for(int y=1; y<10; y++){
       print('$i * $y = ${i*y}');
